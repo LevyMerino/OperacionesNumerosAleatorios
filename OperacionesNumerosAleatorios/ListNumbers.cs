c5b99b7649b9c6677c1aad19588a3d8ec9b960ae
LM <merino.levy@outlook.com>
2023-02-10 16:35:55 -0600

    Agregar archivos de proyecto.

---
+++ OperacionesNumerosAleatorios/ListNumbers.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OperacionesNumerosAleatorios
+{
+    internal class ListNumbers
+    {
+        private List<int> _numbers;
+        private int _n;
+        public string minor;
+        public string higher;
+        public ListNumbers(int n)
+        {
+            _numbers = new List<int>();
+            _n = n;
+            minor = string.Empty;
+            higher = string.Empty;
+        }
+
+        public void RandomNumbers()
+        {
+            Random random = new();
+
+            for (int c = 0; c < _n; c++)
+            {
+                int randomNumber = random.Next(1, 101);
+                _numbers.Add(randomNumber);
+            }
+        }
+
+        public string getInput()
+        {
+            return String.Join(", ", _numbers);
+        }
+
+
+        public string getAverage()
+        {
+
+            float average = _numbers.Sum() / _n;
+            return average.ToString();
+        }
+
+        public string getAddition()
+        {
+            return _numbers.Sum().ToString();
+        }
+
+        public string getDescending()
+        {
+
+            var descendingList = _numbers.OrderByDescending(x => x).ToList();
+            higher = descendingList.First().ToString();
+            return String.Join(", ", descendingList);
+        }
+
+
+        public string getAscending()
+        {
+
+            var ascendingList = _numbers.OrderBy(x => x).ToList();
+            minor = ascendingList.First().ToString();
+            return String.Join(", ", ascendingList);
+        }
+
+    }
+}
---
+++ OperacionesNumerosAleatorios/Program.cs
@@ -0,0 +1,33 @@
+﻿using OperacionesNumerosAleatorios;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Cuantos numeros aleatorios quieres crear");
+        int n = int.Parse(Console.ReadLine());      
+
+        ListNumbers listNumbers = new(n);
+        listNumbers.RandomNumbers();
+        string input = listNumbers.getInput();
+        string average = listNumbers.getAverage();
+        string addition = listNumbers.getAddition();
+        string descending = listNumbers.getDescending();
+        string ascending =  listNumbers.getAscending();
+
+
+        Console.WriteLine($"Entrada: {input}");
+        Console.WriteLine($"Promedio: {average}");
+        Console.WriteLine($"Suma: {addition}");
+        Console.WriteLine($"Mayor: {listNumbers.higher}");
+        Console.WriteLine($"Menor: {listNumbers.minor}");
+        Console.WriteLine($"Ordenamiento Asc: {ascending}");
+        Console.WriteLine($"Ordenamiento Desc: {descending}");
+         
+    }
+
+}
